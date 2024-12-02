cbc29f3f41f3565c9fef3ab7b8850c3fc3119bd2
OnlyAbdullh <abdallaalkasm9@gamil.com>
2024-12-02 17:06:25 +0300

    put the title of swagger there

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
 /**
- * @OA\Info(title="My First API", version="0.1")
+ * @OA\Info(
+ *     title="My API Documentation",
+ *     version="1.0.0",
+ *     description="API documentation for Just-Go-Deliver-App"
+ * )
+ * @OA\Server(
+ *     url="http://localhost:8000/api",
+ *     description="Local development server"
+ * )
  */
 class Controller extends BaseController
 {
