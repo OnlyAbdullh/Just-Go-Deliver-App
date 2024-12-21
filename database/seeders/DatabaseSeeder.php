2a180ff6b5c8acca273c6627b465aa3b37468c6e
Hasan <hzaeter04@gmail.com>
2024-12-21 16:56:10 +0300

    fix bug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Store_Product;
 use Database\Factories\StoreFactory;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
@@ -14,14 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Store_Product::factory(100)->create();
+
         $this->call([
             RolePermessionSeeder::class,
-        /*    StoreSeeder::class,
+            /*    StoreSeeder::class,
             ProductSeeder::class,
             StoreProductSeeder::class,
             CategorySeeder::class,*/
-           // UserSeeder::class
+            // UserSeeder::class
         ]);
+
         // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
