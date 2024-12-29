36915f08666a2eb0361c702de974eb43535120ef
OnlyAbdullh <abdallaalkasm9@gamil.com>
2024-12-29 21:04:34 +0300

    update StockListener to make it use SQL instructions to reduce the queries for decrement the quantities form the store.

--- app/Listeners/UpdateStockListener.php
+++ app/Listeners/UpdateStockListener.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use App\Events\OrderCreated;
 use App\Models\Store_Product;
+use Illuminate\Support\Facades\DB;
+
 class UpdateStockListener
 {
     /**
@@ -19,9 +21,23 @@ class UpdateStockListener
      */
     public function handle(OrderCreated $event)
     {
+        $caseStatements = [];
+        $bindings = [];
+        $ids = [];
+
         foreach ($event->orderProducts as $product) {
-            Store_Product::where('id', $product['store_product_id'])
-                ->decrement('quantity', $product['quantity']);
+            $caseStatements[] = "WHEN id = ? THEN quantity - ?";
+            $bindings[] = $product['store_product_id'];
+            $bindings[] = $product['quantity'];
+            $ids[] = $product['store_product_id'];
         }
+
+        $query = "UPDATE store_products
+              SET quantity = CASE " . implode(' ', $caseStatements) . " END
+              WHERE id IN (" . implode(',', array_fill(0, count($ids), '?')) . ")";
+
+        $bindings = array_merge($bindings, $ids);
+
+        DB::statement($query, $bindings);
     }
 }
