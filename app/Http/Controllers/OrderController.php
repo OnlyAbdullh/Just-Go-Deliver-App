06121c1b4cb5da43a3fa83af0b5fd53b62823f07
OnlyAbdullh <abdallaalkasm9@gamil.com>
2024-12-29 10:06:34 +0300

    make the function of creating orders more efficient.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,6 +8,7 @@ use App\Models\Order_Product;
 use App\Models\Store_Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class OrderController extends Controller
@@ -18,52 +19,57 @@ class OrderController extends Controller
         $user = Auth::user();
 
         $groupedProducts = collect($data)->groupBy('store_id');
-        $storeProductIds = $groupedProducts->flatMap(fn($products) => $products->pluck('store_product_id'));
-        $storeProductDetails = Store_Product::whereIn('id', $storeProductIds)->get()->keyBy('id');
+        $storeProductIds = $groupedProducts->flatMap(fn($products) => $products->pluck('store_product_id'))->unique();
+        $storeProductDetails = Store_Product::whereIn('id', $storeProductIds)->pluck('price', 'id');
 
         $ordersData = [];
         $orderProductsData = [];
         $now = now();
 
+        $orderCounter = 0;
         foreach ($groupedProducts as  $storeProducts) {
-            $totalPrice = $storeProducts->reduce(function ($total, $product) use ($storeProductDetails) {
-                $storeProduct = $storeProductDetails->get($product['store_product_id']);
-                return $total + ($storeProduct->price * $product['quantity']);
-            }, 0);
+            $totalPrice = $storeProducts->sum(function ($product) use ($storeProductDetails) {
+                return $storeProductDetails[$product['store_product_id']] * $product['quantity'];
+            });
 
-            $orderReference = 'ORD-' . $now->format('Ymd-His') . '-' . Str::random(6);
-            $ordersData[] = [
+            $ordersData[$orderCounter] = [
                 'user_id' => $user->id,
                 'total_price' => $totalPrice,
                 'status' => 'pending',
                 'order_date' => $now,
-                'order_reference' => $orderReference,
+                'order_reference' => $this->generateOrderReference(),
                 'created_at' => $now,
                 'updated_at' => $now,
             ];
 
             foreach ($storeProducts as $product) {
-                $storeProduct = $storeProductDetails->get($product['store_product_id']);
                 $orderProductsData[] = [
-                    'store_product_id' => $storeProduct->id,
+                    'store_product_id' => $product['store_product_id'],
                     'quantity' => $product['quantity'],
-                    'price' => $storeProduct->price,
+                    'price' => $storeProductDetails[$product['store_product_id']],
+                    'order_reference_index' => $orderCounter,
                     'created_at' => $now,
                     'updated_at' => $now,
                 ];
             }
+            $orderCounter++;
         }
 
         Order::insert($ordersData);
+        $startingOrderId = Order::max('id') - count($ordersData) + 1;
 
-        $lastInsertedOrderId = Order::max('id') - count($ordersData) + 1;
-        foreach ($orderProductsData as $index => $orderProduct) {
-            $orderProductsData[$index]['order_id'] = $lastInsertedOrderId + floor($index / $groupedProducts->count());
+        foreach ($orderProductsData as &$orderProduct) {
+            $orderProduct['order_id'] = $startingOrderId + $orderProduct['order_reference_index'];
+            unset($orderProduct['order_reference_index']);
         }
 
         Order_Product::insert($orderProductsData);
 
         return JsonResponseHelper::successResponse('Orders created successfully.');
+    }
+    private function generateOrderReference()
+    {
+        return 'ORD-' . now()->format('Ymd-His') . '-' . Str::random(6);
     }
 
 }
